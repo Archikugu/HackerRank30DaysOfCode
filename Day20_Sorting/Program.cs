ff10ed93c9c40c45951a0f01bba21d9c3cab7f6f
Gokhan <engokhangok@gmail.com>
2022-12-12 23:45:59 +0300

    Add project files.

--- Day20_Sorting/Program.cs
+++ Day20_Sorting/Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 
 namespace Day20_Sorting
 {
+    /*
+     Task
+        Given an array, a, of size n distinct elements, sort the array in ascending order usinng
+        the Bubble Sort algorithm above. Once sorted, print the following 3 lines:
+        1. Array is sorted in numSwaps swaps.
+        where numSwaps is the number of swaps that took place.
+        2. First Element: firstElement
+        where firstElement is the first element in the sorted array.
+        3. Last Element: lastElement
+        where  is the last element in the sorted array.
+        Hint: To complete this challenge, you will need to add a variable that keeps a running tally of all swaps that occur during execution.
+     */
     internal class Program
     {
 
